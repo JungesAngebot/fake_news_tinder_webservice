b620c7b9b1ebfb3156f4fd0f404a57bea8bcff83
Christoph Eberhardt <christoph@christoph-mbp.local>
2017-04-09 09:23:34 +0200

    basic question handling

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -2,5 +2,8 @@ class QuizController < ApplicationController
   layout 'quiz'
 
   def quiz
+    c = ApplicationController.new
+    @quiz_json = c.render_to_string( partial: '/api/v1/quizzes/show.json.jbuilder', locals: { quiz: Quiz.first}).html_safe
+
   end
 end
